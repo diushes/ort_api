dfbfe725e6b823d343cd7350294a530910758458
Diushes <diushes1712@gmail.com>
2023-05-21 14:04:19 +0600

    some changes

--- config/settings.py
+++ config/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         "NAME": "ortdb",
         "USER": "postgres",
         "PASSWORD": "diushes",
-        "HOST": "127.0.0.1",
+        "HOST": "localhost",
         "PORT": "5432",
     }
 }
