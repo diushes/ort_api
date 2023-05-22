651912356b4786e5e229037b1e915dec6a18f284
Diushes <diushes1712@gmail.com>
2023-05-23 00:56:31 +0600

    added endpoints

--- tests/urls.py
+++ tests/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TestListView
+from .views import TestListView, TestsByCategoryListView
 
 urlpatterns = [
     path("tests/", TestListView.as_view(), name="tests"),
+    path('testsbycategory/<int:category_id>/', TestsByCategoryListView.as_view(), name='tests-by-category-list'),
 ]
--- tests/views.py
+++ tests/views.py
@@ -6,3 +6,10 @@ from .serializers import TestSerializer
 class TestListView(generics.ListAPIView):
     queryset = Test.objects.all()
     serializer_class = TestSerializer
+
+class TestsByCategoryListView(generics.ListAPIView):
+    serializer_class = TestSerializer
+
+    def get_queryset(self):
+        category_id = self.kwargs['category_id']  # Assuming category_id is passed as a URL parameter
+        return Test.objects.filter(category=category_id)
