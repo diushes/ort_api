e6c67927556e8854e27a29845e9102ad30caab69
Diushes <diushes1712@gmail.com>
2023-05-24 18:49:49 +0600

    changed to LOGIN_REDIRECT

--- config/settings.py
+++ config/settings.py
@@ -183,4 +183,5 @@ AUTHENTICATION_BACKENDS = [
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 SOCIAL_AUTH_GOOGLE_OAUTH2_REDIRECT_URI = 'https://ort.herokuapp.com/accounts/google/login/callback/'
+                                        
 
