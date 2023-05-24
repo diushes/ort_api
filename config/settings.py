ff1ba6f31d4d2dcbc867a1b6208842f1555d361b
Diushes <diushes1712@gmail.com>
2023-05-24 18:52:26 +0600

    forgot to save

--- config/settings.py
+++ config/settings.py
@@ -182,6 +182,6 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
-SOCIAL_AUTH_GOOGLE_OAUTH2_REDIRECT_URI = 'https://ort.herokuapp.com/accounts/google/login/callback/'
+LOGIN_REDIRECT_URl = 'https://ort.herokuapp.com/accounts/google/login/callback/'
                                         
 
