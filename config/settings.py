f5ccb84e7f8e477178c63d50f2e3e6de1c41ae2a
Diushes <diushes1712@gmail.com>
2023-05-29 00:48:39 +0600

    addded utf-8

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+DEFAULT_CHARSET = 'utf-8'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-r-cu-@9+j^v_2l%fks#9wnsl+5lhl4i_%93+qya&etz_@go6!u"
