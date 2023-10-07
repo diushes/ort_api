a27bf633f171401f82753fecccd062278219b098
Diushes <diushes1712@gmail.com>
2023-10-07 16:50:18 +0600

    edited serializers fields

--- tests/serializers.py
+++ tests/serializers.py
@@ -20,6 +20,7 @@ class QuestionSerializer(serializers.ModelSerializer):
 
 class TestSerializer(serializers.ModelSerializer):
     questions = QuestionSerializer(many=True, read_only=True)
+    category_title = serializers.CharField(source='category.title', read_only=True)
 
     class Meta:
         model = Test
